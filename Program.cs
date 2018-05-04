9731e1bba4f8bea37360149a77cfb4da63ae7816
MHY Express <mcyalda@yahoo.com>
2018-05-04 11:54:17 -0400

    1st commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arQam = { 1, 2, 3, 4, 5 };
+            int maxNumber = 10;
+
+           bool result =  IsSumLarger(arQam, maxNumber);
+            Console.WriteLine(result);
+        }
+        #region
+        public static bool IsSumLarger(int[] numbers, int comparisonNumber)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i]++;
+                sum += numbers[i];
+            }
+            if (sum > comparisonNumber)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
